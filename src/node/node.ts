ee12312a94cf23f7394898c0dfe2a1a3f2d9bcbb
James Monger <jameskmonger@hotmail.co.uk>
2022-07-17 12:30:23 +0100

    feat: create Node and Synapse structures

--- src/node/node.ts
+++ src/node/node.ts
@@ -1,3 +1,54 @@
-export type Node = {
-    innovation: number;
+export enum ActivationFunction {
+	CONSTANT = "constant",
+	SIGMOID = "sigmoid",
+	LINEAR = "linear",
+	TANH = "tanh",
+	SIN = "sin",
+	RELU = "relu",
+	GAUSSIAN = "gaussian",
+	LATCH = "latch",
+	DIFF = "diff",
 }
+
+export type Node = {
+	/**
+	 * The type of activation function to use for this node
+	 */
+	activation: ActivationFunction;
+
+	/**
+	 * This nodes unique index
+	 */
+	index: number;
+
+	/**
+	 * Used to give this node a uniquer identifier based on when it was created.
+	 *
+	 * This can be used to track genes between generations.
+	 */
+	innovation: number;
+
+	/**
+	 * A description of this node
+	 */
+	description: string;
+
+	/**
+	 * The current value of this node
+	 */
+	value: number;
+
+	/**
+	 * The last input value of this node
+	 *
+	 * This is used for the Differential activation function
+	 */
+	lastInput: number;
+
+	/**
+	 * The last output value of this node
+	 *
+	 * This is used for the Latch activation function
+	 */
+	lastOutput: number;
+};
--- src/synapse/synapse.ts
+++ src/synapse/synapse.ts
@@ -1,3 +1,23 @@
 export type Synapse = {
-    innovation: number;
-}
+	/**
+	 * Used to give this node a uniquer identifier based on when it was created.
+	 *
+	 * This can be used to track genes between generations.
+	 */
+	innovation: number;
+
+	/**
+	 * The index of the input node this synapse is connected to
+	 */
+	nodeIn: number;
+
+	/**
+	 * The index of the output node this synapse is connected to
+	 */
+	nodeOut: number;
+
+	/**
+	 * The weight of this synapse
+	 */
+	weight: number;
+};
